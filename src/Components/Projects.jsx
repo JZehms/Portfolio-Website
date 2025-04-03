6274b8221ca0475d58f04c7fd84720030bd8a700
Joshua <joshuazehms@gmail.com>
2025-04-03 14:39:41 -0700

    Small projects updates

---
+++ src/Components/Projects.jsx
@@ -0,0 +1,13 @@
+import "./Component-Styles/Projects.css";
+
+function Projects() {
+  return (
+    <div className="projects-container">
+      <section id="projects">
+      <h2 className="projects-title">My projects</h2>
+      </section>
+    </div>
+  );
+}
+
+export default Projects;
