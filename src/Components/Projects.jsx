be3498d4ca05d1c5ec6bf2e10e5f0cf069d085ed
Joshua <joshuazehms@gmail.com>
2025-04-04 14:48:16 -0700

    Projects updates, and fixed react icon animation

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -4,7 +4,66 @@ function Projects() {
   return (
     <div className="projects-container">
       <section id="projects">
-      <h2 className="projects-title">My projects</h2>
+        <h2 className="projects-title">My projects</h2>
+        <div className="projects-body">
+          <div className="project-1">
+            <div className="project-images">
+              <img
+                className="project-1-image"
+                alt="Homepage Work at DHI Group"
+                src="src/assets/Projects-Assets/Crawler-Placeholder.png"
+              ></img>
+            </div>
+            <div className="projects-text">
+              <p>
+                Morbi in sem quis dui placerat ornare. Pellentesque odio nisi
+                euismod in pharetra a ultricies in diam. Sed arcu. Cras
+                consequat. Praesent dapibus neque id cursus faucibus tortor
+                neque egestas auguae eu vulputate magna eros eu erat. Aliquam
+                erat volutpat. Nam dui mi tincidunt quis accumsan porttitor
+                facilisis luctus metus.
+              </p>
+            </div>
+          </div>
+          <div className="project-1">
+            <div className="project-images">
+              <img
+                className="project-1-image"
+                alt="Homepage Work at DHI Group"
+                src="src/assets/Projects-Assets/Homepage-Recording.gif"
+              ></img>
+            </div>
+            <div className="projects-text">
+              <p>
+                Morbi in sem quis dui placerat ornare. Pellentesque odio nisi
+                euismod in pharetra a ultricies in diam. Sed arcu. Cras
+                consequat. Praesent dapibus neque id cursus faucibus tortor
+                neque egestas auguae eu vulputate magna eros eu erat. Aliquam
+                erat volutpat. Nam dui mi tincidunt quis accumsan porttitor
+                facilisis luctus metus.
+              </p>
+            </div>
+          </div>
+          <div className="project-1">
+            <div className="project-images">
+              <img
+                className="project-1-image"
+                alt="Homepage Work at DHI Group"
+                src="src/assets/Projects-Assets/Homepage-Recording-Resized-2.gif"
+              ></img>
+            </div>
+            <div className="projects-text">
+              <p>
+                Morbi in sem quis dui placerat ornare. Pellentesque odio nisi
+                euismod in pharetra a ultricies in diam. Sed arcu. Cras
+                consequat. Praesent dapibus neque id cursus faucibus tortor
+                neque egestas auguae eu vulputate magna eros eu erat. Aliquam
+                erat volutpat. Nam dui mi tincidunt quis accumsan porttitor
+                facilisis luctus metus.
+              </p>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
