2d5fd652cfb7e52d3fd673e789fbe3d5c566e97d
Joshua <joshuazehms@gmail.com>
2025-04-27 16:53:25 -0700

    Projects text shortening

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -16,7 +16,7 @@ function Projects() {
               <img
                 className="project-1-image"
                 alt="Homepage Work at DHI Group"
-                src= {webScraper}
+                src={webScraper}
               ></img>
             </div>
             <div className="projects-text-container">
@@ -50,7 +50,7 @@ function Projects() {
               <img
                 className="project-1-image"
                 alt="Homepage Work at DHI Group"
-                src= {dhiGif}
+                src={dhiGif}
                 loading="lazy"
               ></img>
             </div>
@@ -63,22 +63,17 @@ function Projects() {
                 <span className="aws-style">AWS</span>
               </div>
               <p className="projects-text">
-                During my time at DHI Group, I was part of the Content
-                Management Team, where I was responsible for{" "}
+                At DHI Group, I helped the Content Management Team by{" "}
                 <span className="projects-inverse">
                   translating Figma designs into reusable React components and
-                  building entire pages for the website.
+                  building pages
                 </span>{" "}
-                My work involved a combination of React, JavaScript, Java, HTML,
-                and CSS to deliver dynamic and user-friendly content. One of my
-                proudest accomplishments was designing and developing the
-                company’s landing page.{" "}
+                using React, JavaScript, Java, HTML, and CSS. One highlight was{" "}
                 <span className="projects-inverse">
-                  It was a key project since it was the first impression
-                  visitors would have when they arrived at the site,
+                  designing and developing the company's new landing page,
                 </span>{" "}
-                and I was focused on ensuring it was visually appealing and
-                highly functional.
+                ensuring it made a strong first impression with a clean,
+                user-friendly design.
               </p>
               <div className="view-project-button">
                 <a href="https://www.dice.com/">
@@ -92,7 +87,7 @@ function Projects() {
               <img
                 className="project-1-image"
                 alt="Homepage Work at DHI Group"
-                src= {portfolioGif}
+                src={portfolioGif}
                 loading="lazy"
               ></img>
             </div>
@@ -103,22 +98,19 @@ function Projects() {
                 <span className="js-style">JavaScript</span>
               </div>
               <p className="projects-text">
-                This is the page you are currently viewing.{" "}
+                This is the page you're currently viewing.{" "}
                 <span className="projects-inverse">
-                  My goal with this project was to expand my knowledge and
-                  skills in ReactJS while creating something that effectively
-                  showcases my past and present work.
+                  I built it to grow my ReactJS skills and create a space to
+                  showcase my work.
                 </span>{" "}
-                I spent a significant amount of time designing and building this
-                page, ensuring it not only highlights the projects I've worked
-                on but also reflects my growth as a developer. Each section of
-                this page is a{" "}
+                I spent a lot of time designing and building it to highlight my
+                projects and{" "}
                 <span className="projects-inverse">
-                  direct reflection of the technologies and techniques I've
-                  learned along the way.
+                  reflect my growth as a developer.
                 </span>{" "}
-                If you're interested in exploring the code behind it, feel free
-                to click the button below to view the source code.
+                Each section demonstrates the technologies and techniques I've
+                learned. If you’d like to explore the code, click below to view
+                the source.
               </p>
               <div className="view-project-button">
                 <a href="https://github.com/JZehms/Portfolio-Website">
@@ -132,7 +124,7 @@ function Projects() {
               <img
                 className="project-1-image"
                 alt="Homepage Work at DHI Group"
-                src= {weatherImg}
+                src={weatherImg}
               ></img>
             </div>
             <div className="projects-text-container">
@@ -146,19 +138,16 @@ function Projects() {
               <p className="projects-text">
                 This is my{" "}
                 <span className="projects-inverse">current project.</span> I'm
-                building a weather website to deepen my understanding of
-                ReactJS, Vite, and how to integrate React Query for working with
-                APIs. Along with gaining experience In frontend technologies,
-                I'm also working with Node.js and Express will fetch and manage
-                data from the API.{" "}
+                building a weather website to deepen my knowledge of ReactJS,
+                Vite, and React Query for API integration. Along with frontend
+                experience, I'm using Node.js and Express to fetch and manage
+                API data.{" "}
                 <span className="projects-inverse">
-                  My end goal is to transform this website into a mobile app
-                  using React Native, which will give me hands-on experience
-                  with cross-platform development.
+                  My goal is to turn this into a mobile app with React Native,
+                  gaining experience in cross-platform development.
                 </span>{" "}
-                By doing so, I’ll be able to learn how to adapt my web app into
-                a fully functional mobile application, further expanding my
-                development skills across both web and mobile platforms.
+                By doing this, I’ll learn how to adapt the web app into a mobile
+                app, expanding my skills in both web and mobile platforms.
               </p>
               <div className="view-project-button">
                 <button className="project-button">In Progress</button>
