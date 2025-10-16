9341c0087e41a1cb419f7c210206a68806df45df
Joshua <joshuazehms@gmail.com>
2025-10-16 13:51:39 -0700

    Missing font-awesome import

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Projects from "./Components/Projects";
 import Contact from "./Pages/Contact";
 import Footer from "./Components/Footer";
 import "./styles/App.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function App() {
   return (
